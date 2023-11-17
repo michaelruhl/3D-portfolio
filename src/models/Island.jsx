4fb98a404ed0b9c617152a8f076f32ed10126e06
Michael Ruhl <michaelruhl14@hotmail.com>
2023-11-17 00:13:19 -0500

    add math for island rotation

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -24,7 +24,7 @@ const Island = ({isRotating, setIsRotating, ...props}) => {
     const dampingFactor = 0.95
 
     const handlePointerDown = (e) => {
-        e.stopProgation()
+        e.stopPropagation()
         e.preventDefault()
 
         setIsRotating(true)
@@ -35,30 +35,107 @@ const Island = ({isRotating, setIsRotating, ...props}) => {
     }
 
     const handlePointerUp = (e) => {
-        e.stopProgation()
+        e.stopPropagation()
         e.preventDefault()
 
         setIsRotating(false)
-    }
-
-    const handlePointerMove = (e) => {
-        e.stopProgation()
-        e.preventDefault()
 
         
     }
+
+    const handlePointerMove = (e) => {
+        e.stopPropagation()
+        e.preventDefault()
+
+        if(isRotating) {
+            const clientX = e.touches ? e.touches[0].clientX : e.clientX
+
+        const delta = (clientX - lastX.current) / viewport.width 
+
+        islandRef.current.rotation.y += delta * 0.01 * Math.PI
+
+        lastX.current = clientX
+
+        rotationSpeed.current = delta * 0.01 * Math.PI
+        }
+        
+    }
+
+    const handleKeyDown = (e) => {
+        if(e.key === 'ArrowLeft') {
+        if(!isRotating) setIsRotating(true)
+        islandRef.current.rotation.y += 0.01 * Math.PI
+        } else if (e.key === 'ArrowRight') {
+        if(!isRotating) setIsRotating(true)
+        islandRef.current.rotation.y -= 0.01 * Math.PI
+
+    }
+}
+
+    const handleKeyUp = (e) => {
+        if(e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+            setIsRotating(false)
+        }
+    }
+
+    useFrame(() => {
+        if(!isRotating) {
+            rotationSpeed.current *= dampingFactor
+            if(Math.abs(rotationSpeed.cirrent) < 0.001) {
+                rotationSpeed.current = 0
+            }
+
+            islandRef.current.rotation.y += rotationSpeed.current
+        } else {
+            const rotation = islandRef.current.rotation.y
+
+            const normalizedRotation =
+        ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+      // Set the current stage based on the island's orientation
+      switch (true) {
+        case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+          setCurrentStage(4);
+          break;
+        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+          setCurrentStage(3);
+          break;
+        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+          setCurrentStage(2);
+          break;
+        case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+          setCurrentStage(1);
+          break;
+        default:
+          setCurrentStage(null);
+      }
+        }
+    })
+
+    useEffect(() => {
+        const canvas = gl.domElement
+        canvas.addEventListener('pointerdown', handlePointerDown)
+        canvas.addEventListener('pointermove', handlePointerMove)
+        canvas.addEventListener('pointerup', handlePointerUp)
+        document.addEventListener('keydown', handleKeyDown)
+        document.addEventListener('keyup', handleKeyUp)
+
+        return () => {
+            canvas.removeEventListener('pointerdown', handlePointerDown)
+            canvas.removeEventListener('pointermove', handlePointerMove)
+            canvas.removeEventListener('pointerup', handlePointerUp)
+            document.addEventListener('keydown', handleKeyDown)
+        document.addEventListener('keyup', handleKeyUp)
+        }
+    },[gl, handlePointerDown, handlePointerMove, handlePointerUp])
 
   return (
     <a.group ref={islandRef} {...props}>
       <mesh
-        
-        
         geometry={nodes.polySurface944_tree_body_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        
-        
         geometry={nodes.polySurface945_tree1_0.geometry}
         material={materials.PaletteMaterial001}
       />
