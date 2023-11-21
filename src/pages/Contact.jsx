94473a49b96c5f55c7d3b801412dc079b8cf68dd
Michael Ruhl <michaelruhl14@hotmail.com>
2023-11-21 07:29:35 -0500

    fix background color on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -60,7 +60,7 @@ const Contact = () => {
   const handleBlur = () => setCurrentAnimation('idle');
 
   return (
-    <section className="relative flex lg:flex-row flex-col max-container">
+    <section className="relative flex lg:flex-row h-screen flex-col max-container">
       {alert.show && <Alert {...alert} />}
 
 
